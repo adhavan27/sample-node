06f98181aa4d17af215368d063afdcf80e875ae1
adhavan27 <105037638+adhavan27@users.noreply.github.com>
2022-09-28 15:54:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('<h2 style="color: green"> Unimity Assessment Output<h2>');
+app.get('/sample', (req, res) => {
+  res.send('<h2 style="color: red"> Unimity Assessment Output<h2>');
 });
 
 app.listen(PORT, HOST);
